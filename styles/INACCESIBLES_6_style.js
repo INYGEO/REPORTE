c8380cd38c26e4a8ce7e69dc4732bb69e3e7dab4
INYGEO <inygeo@hotmail.com>
2025-04-07 16:48:24 -0700

    Add files via upload

--- styles/INACCESIBLES_6_style.js
+++ styles/INACCESIBLES_6_style.js
@@ -1,26 +1,26 @@
 var size = 0;
 var placement = 'point';
 
-var style_MALLA_TIJUANA_1 = function(feature, resolution){
+var style_INACCESIBLES_6 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
     };
     
     var labelText = ""; var value = feature.get("");
-    var labelFont = "13.0px \'Rockwell Extra Bold\', sans-serif";
-    var labelFill = "#ffe500";
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
     var bufferColor = "";
     var bufferWidth = 0;
     var textAlign = "left";
     var offsetX = 0;
     var offsetY = 0;
     var placement = 'point';
-    if (feature.get("DIA") !== null) {
-        labelText = String(feature.get("DIA"));
+    if ("" !== null) {
+        labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(255,0,6,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,0,3,0.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.7)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,17,0,0.7)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
